38d893c34df6b4a0c2afcc3531a4bb9b683f14e6
Carlos Zabala <czabala847@gmail.com>
2024-04-11 16:19:59 -0500

    Configurando alias en el proyecto

--- src/pages/public/auth/presentation/Login.page.jsx
+++ src/pages/public/auth/presentation/Login.page.jsx
@@ -1,9 +1,11 @@
-
+import { Test } from "@/common/presentation/components/Test";
 
 export const LoginPage = () => {
   return (
     <div>
       <h1>Hola mundo</h1>
+
+      <Test />
     </div>
-  )
-}
+  );
+};
