82a2ca3a6d27b6e254972e66035f3381b29eabad
unknown <backocw>
2024-06-17 16:38:49 -0500

    add search-bar

--- src/ecommerce/common/presentation/components/searchBar/Search.jsx
+++ src/ecommerce/common/presentation/components/searchBar/Search.jsx
@@ -1,43 +1,111 @@
-import React, { useEffect, useState } from "react";
-import { Select } from 'antd';
+import React, { useRef } from "react";
 import Styles from './scss/search.module.scss';
 import { useSearch } from "./view-model/useSearch";
 
-import AuthProducts from "../../../../auth/products/AuthProducts"
-
 const SearchBar = () => {
-    // const {   } = useSearch();
-    const [listProducts, setListProducts] = useState([]);
+    const { getProductsSearch, listProducts, loadingProducts, isSearching, findFilter, onClickOutside, configPages } = useSearch();
+    const ref = useRef(null)
+    const handleSearch = () => {
+        document.querySelector('.navmenu').classList.add('active-search')
+        document.querySelector('.input-search').classList.add('active-search')
+        ref.current.focus()
+      }
 
-    const onChange = (value) => {
-        console.log(`selected ${value}`);
-    };
+      const handleOnBlur = () => {
+        document.querySelector('.navmenu').classList.remove('active-search')
+        document.querySelector('.input-search').classList.remove('active-search')
+      }
 
-    const getProductsSearch = async (value) => {
-        console.log(value);
-        if (value.length < 3) return
-        else {
-            try {
-                let result = await AuthProducts('-1', value);
-                console.log(result);
-                setListProducts(result);
-            } catch (error) {
-                console.error(error);
-            }
-        }
-    }
+      console.log(configPages.images.no_found_product)
 
     return (
-        <input onChange={e => getProductsSearch(e.target.value)}>
+        <div>
+            <div role="button" onClick={handleSearch}>
+                <input
+                    type="text"
+                    placeholder="Buscar producto..."
+                    name="search"
+                    id="search"
+                    ref={ref}
+                    onChange={(e) => getProductsSearch(e.target.value)}
+                    value={findFilter}
+                    onBlur={handleOnBlur}
+                ></input>
+                
+            </div>
+            {isSearching ? (
+                <div className={Styles.searchFinderBackdrop}>
+                    <div className={Styles.dropFinder} onClick={onClickOutside}></div>
 
+                    <div className={Styles.finderSearch}>
+                        <ul>
+                            {listProducts.length > 0 && loadingProducts === false ? (
+                                listProducts?.map((product) => (
+                                    <div>
+                                        <li key={product.item.code}>
+                                            <div className={Styles.productContainer}>
+                                                <div>
+                                                    <img
+                                                        width={80}
+                                                        heigh={80}
+                                                        src={product.item.imagen}
+                                                        onError={(e) => {
+                                                            e.target.src = configPages.images.no_found_product
+                                                        }}
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <p>{product.item.title}</p>
+                                                    <p>Código: {product.item.code}</p>
+                                                    {product.item.countSuggested ? (
+                                                        <div>Cantidad Sugerida : {product.item.countSuggested}</div>
+                                                    ) : null}
+                                                    <b>
+                                                        {' $' +
+                                                            new Intl.NumberFormat('en', {
+                                                                numberingSystem: 'latn',
+                                                                style: 'decimal',
+                                                                currency: 'COP'
+                                                            }).format(
+                                                                parseFloat(product.item.tax) > 0
+                                                                    ? Math.round(
+                                                                        (parseFloat(product.item.price) *
+                                                                            parseFloat(product.item.tax)) /
+                                                                        100 +
+                                                                        parseFloat(product.item.price)
+                                                                    )
+                                                                    : product.item.price
+                                                            ) +
+                                                            (product.item.units === 'GR' ? ' x Gramo' : '')}
+                                                    </b>
+                                                </div>
+                                                <div>
+                                                    <div >
+                                                        <div>
+                                                            <button> - </button>
+                                                        </div>
+                                                        <div>
+                                                            <input
+                                                                type="number"
 
+                                                            />
+                                                        </div>
+                                                        <button > + </button>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </li>
+                                    </div>
+                                ))
+                            ) : (
+                                <p>Buscando ...</p>
+                            )}
+                        </ul>
+                    </div>
+                </div>
+            ) : null}
 
-
-
-
-
-        </input>
-
+        </div>
 
 
     )
--- src/ecommerce/common/presentation/components/searchBar/view-model/useSearch.js
+++ src/ecommerce/common/presentation/components/searchBar/view-model/useSearch.js
@@ -1,25 +1,60 @@
 import React, { useState } from "react";
 import AuthProducts from "../../../../../auth/products/AuthProducts"
+import Fuse from "fuse.js";
+import { useEcommerceStore } from "../../../../infrastructure/store";
 
 export const useSearch = () => {
-
+  const { configPages } = useEcommerceStore();
+  const [isSearching, setIsSearching] = useState(false)
   const [listProducts, setListProducts] = useState([]);
-  const getProductsSearch = async (value) => {
-    console.log(value);
-    if (value.length < 3) return
-    else {
-      try {
-        let result = await AuthProducts('-1', value);
-        console.log(result);
-        setListProducts(result);
-      } catch (error) {
-        console.error(error);
-      }
+  const [loadingProducts, setLoadingProducts] = useState(false)
+  const [findFilter, setFindFilter] = useState('')
+  let searchProduct = []
+
+  const getProductsSearch  = (productSearching) => {
+    const options = {
+      includeScore: true,
+      keys: [
+        { name: 'code', weight: 0.7 },
+        { name: 'title', weight: 0.3 }
+      ]
+    }
+    searchProduct.push(productSearching)
+    setFindFilter(productSearching)
+    if (productSearching !== '' && productSearching.length > 0) {
+      setLoadingProducts(true)
+      setTimeout(async () => {
+        
+        let result = await AuthProducts('-1', '-1');
+        const fuse = new Fuse(result, options)
+        const resultSearch = fuse.search(productSearching)
+        if (result) {
+          setListProducts(resultSearch)
+          setLoadingProducts(false)
+        } else {
+          setLoadingProducts(false)
+        }
+      }, 500)
+      setIsSearching(true)
+    } else if (productSearching === '') {
+      setListProducts({})
+      setIsSearching(false)
     }
   }
+
+  const onClickOutside = () => {
+    setFindFilter('')
+    setIsSearching(false)
+  }
+
   return {
     getProductsSearch,
-    listProducts
+    listProducts,
+    isSearching,
+    loadingProducts,
+    findFilter,
+    onClickOutside,
+    configPages
   }
 }
 
