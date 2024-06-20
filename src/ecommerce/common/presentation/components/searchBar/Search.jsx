d63bcdab4894016ff48e509bceed2ab81503542d
Carlos Zabala <czabala847@gmail.com>
2024-06-20 11:15:08 -0500

    fix search bar

--- src/ecommerce/common/presentation/components/searchBar/Search.jsx
+++ src/ecommerce/common/presentation/components/searchBar/Search.jsx
@@ -1,4 +1,7 @@
-import { InputField } from "../../../../../common/presentation/components"
+import {
+  InputField,
+  Loading,
+} from "../../../../../common/presentation/components"
 import { CardProduct } from "../../../../pages/public/products/presentation/components"
 import Styles from "./scss/search.module.scss"
 import { useSearch } from "./view-model/useSearch"
@@ -11,27 +14,14 @@ const SearchBar = () => {
     isSearching,
     findFilter,
     onClickOutside,
-    configPages,
   } = useSearch();
-
-  // const ref = useRef(null);
-
-  // const handleSearch = () => {
-  // document.querySelector(".navmenu").classList.add("active-search");
-  // document.querySelector(".input-search").classList.add("active-search");
-  // ref.current.focus();
-  // };
-
-  // const handleOnBlur = () => {
-  // document.querySelector(".navmenu").classList.remove("active-search");
-  // document.querySelector(".input-search").classList.remove("active-search");
-  // };
 
   return (
     <div>
       {/* <div onClick={handleSearch}> */}
 
       <InputField
+        inputAutocomplete={false}
         className={Styles.InputSearch}
         placeholder="Buscar producto..."
         name="search"
@@ -39,93 +29,40 @@ const SearchBar = () => {
         value={findFilter}
       />
 
-      {/* <input
-        type="text"
-        placeholder="Buscar producto..."
-        name="search"
-        id="search"
-        ref={ref}
-        onChange={(e) => getProductsSearch(e.target.value)}
-        value={findFilter}
-        onBlur={handleOnBlur}
-      /> */}
-      {/* </div> */}
-      {isSearching ? (
+      {isSearching && (
         <div className={Styles.searchFinderBackdrop}>
           <div className={Styles.dropFinder} onClick={onClickOutside}></div>
 
           <div className={Styles.finderSearch}>
-            <ul>
-              {listProducts.length > 0 && loadingProducts === false ? (
-                listProducts?.slice(0, 5).map((product, index) => (
+            {listProducts.length > 0 &&
+              loadingProducts === false &&
+              listProducts?.map((product, index) => (
+                <div
+                  className={Styles.ProductItem}
+                  key={`${product.code}-${index}`}
+                >
                   <CardProduct
+                    sizeCardRow="small"
+                    type="horizontal"
                     product={product}
-                    key={`${product.code}-${index}`}
                   />
+                </div>
+              ))}
 
-                  // <div key={`${product.code}-${index}`}>
-                  //   <li key={product.item.code}>
-                  //     <div className={Styles.productContainer}>
-                  //       <div>
-                  //         <img
-                  //           width={80}
-                  //           height={80}
-                  //           src={product.item.imagen}
-                  //           onError={(e) => {
-                  //             e.target.src =
-                  //               configPages.images.no_found_product;
-                  //           }}
-                  //         />
-                  //       </div>
-                  //       <div>
-                  //         <p>{product.item.title}</p>
-                  //         <p>Código: {product.item.code}</p>
-                  //         {product.item.countSuggested ? (
-                  //           <div>
-                  //             Cantidad Sugerida : {product.item.countSuggested}
-                  //           </div>
-                  //         ) : null}
-                  //         <b>
-                  //           {" $" +
-                  //             new Intl.NumberFormat("en", {
-                  //               numberingSystem: "latn",
-                  //               style: "decimal",
-                  //               currency: "COP",
-                  //             }).format(
-                  //               parseFloat(product.item.tax) > 0
-                  //                 ? Math.round(
-                  //                     (parseFloat(product.item.price) *
-                  //                       parseFloat(product.item.tax)) /
-                  //                       100 +
-                  //                       parseFloat(product.item.price)
-                  //                   )
-                  //                 : product.item.price
-                  //             ) +
-                  //             (product.item.units === "GR" ? " x Gramo" : "")}
-                  //         </b>
-                  //       </div>
-                  //       <div>
-                  //         <div>
-                  //           <div>
-                  //             <button> - </button>
-                  //           </div>
-                  //           <div>
-                  //             <input type="number" />
-                  //           </div>
-                  //           <button> + </button>
-                  //         </div>
-                  //       </div>
-                  //     </div>
-                  //   </li>
-                  // </div>
-                ))
-              ) : (
-                <p>Buscando ...</p>
-              )}
-            </ul>
+            {loadingProducts && (
+              <div>
+                <Loading />
+              </div>
+            )}
+
+            {!loadingProducts && listProducts.length === 0 && (
+              <div className={Styles.ProductNotFound}>
+                <p>No se encontraron resultados!</p>
+              </div>
+            )}
           </div>
         </div>
-      ) : null}
+      )}
     </div>
   );
 };
